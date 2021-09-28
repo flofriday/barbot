d95c64e63a761f21fb506d6f2671c1eb30534e7f
flofriday <flohacksfriday@gmail.com>
2021-09-28 15:41:01 +0200

    Add first experiment with three.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # barbot
 Software for a barbot
+
+Maybe a bit overengineered but, why wouldn't a automate use 3d rendering ;)
+
+Work in progress ... duh
---
+++ index.js
@@ -0,0 +1,40 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.CylinderGeometry(0.7, 0.5, 2, 32);
+//const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+const cup = new THREE.Mesh( geometry, material );
+cup.rotation.x = 0.4;
+scene.add( cup );
+
+camera.position.z = 5;
+
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5,5,4)
+scene.add(pointLight)
+const ambientLight = new THREE.AmbientLight(0x404040)
+scene.add(ambientLight)
+
+
+// ANIMATE
+
+const clock = new THREE.Clock();
+
+function animate() {
+    const elapsed = clock.getElapsedTime();
+
+    cup.rotation.y = 0.5 * elapsed;
+    cup.position.y = Math.sin(elapsed) * 0.6;
+    cup.rotation.z = Math.sin(elapsed) * 0.3;
+    
+
+	renderer.render( scene, camera );
+	requestAnimationFrame( animate );
+}
+animate();
